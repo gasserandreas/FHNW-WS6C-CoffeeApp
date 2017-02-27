4e8b3689dfbedde2c2df4e1c170e2b742990c425
Andreas Gasser <andreas.safe@gmail.com>
2017-02-27 16:27:45 +0100

    added empty project

---
+++ FHNW-WS6C-CoffeeApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FHNW-WS6C-CoffeeApp
+//
+//  Created by Andreas Gasser on 27.02.17.
+//  Copyright © 2017 FHNW. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
